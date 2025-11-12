3d7107fbc843a9496934caf9f1c449bd199f0769
romiksargsyan-beep <romik.sargsyan@picsart.academy>
2025-11-12 18:58:22 +0400

    changes

--- credentials/PicsartApi.credentials.ts
+++ credentials/PicsartApi.credentials.ts
@@ -1,9 +1,9 @@
-import { ICredentialType, INodeProperties } from 'n8n-workflow';
+import { IAuthenticateGeneric, ICredentialTestRequest, ICredentialType, INodeProperties } from 'n8n-workflow';
 
 export class PicsartApi implements ICredentialType {
 	name = 'picsartApi';
 	displayName = 'Picsart API';
-	documentationUrl = 'https://docs.picsart.io'; // 👈 Add a valid docs link
+	documentationUrl = 'https://docs.picsart.io/docs/getting-started-with-picsart-api';
 
 	properties: INodeProperties[] = [
 		{
@@ -11,9 +11,32 @@ export class PicsartApi implements ICredentialType {
 			name: 'apiKey',
 			type: 'string',
 			default: '',
+			required: true,
 			typeOptions: {
 				password: true,
 			},
+			description: 'Your Picsart API key. You can get it from https://console.picsart.io/dashboard',
 		},
 	];
+
+	authenticate: IAuthenticateGeneric = {
+		type: 'generic',
+		properties: {
+			headers: {
+				'x-picsart-api-key': '={{$credentials.apiKey}}',
+			},
+		},
+	};
+
+	// The block below tells how this credential can be tested
+	test: ICredentialTestRequest = {
+		request: {
+			baseURL: 'https://api.picsart.io',
+			url: '/tools/1.0/balance',
+			method: 'GET',
+			headers: {
+				'accept': 'application/json',
+			},
+		},
+	};
 }
