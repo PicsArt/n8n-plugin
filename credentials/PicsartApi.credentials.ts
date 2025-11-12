fcd12c7d05be757b4529bb8ba7c7b3f30182e80f
romiksargsyan-beep <romik.sargsyan@picsart.academy>
2025-11-12 11:12:16 +0400

    Save current changes before restructuring

--- credentials/PicsartApi.credentials.ts
+++ credentials/PicsartApi.credentials.ts
@@ -3,7 +3,7 @@ import { ICredentialType, INodeProperties } from 'n8n-workflow';
 export class PicsartApi implements ICredentialType {
 	name = 'picsartApi';
 	displayName = 'Picsart API';
-	documentationUrl = 'https://docs.picsart.io/reference/image-upscale'; // 👈 Add a valid docs link
+	documentationUrl = 'https://docs.picsart.io'; // 👈 Add a valid docs link
 
 	properties: INodeProperties[] = [
 		{
