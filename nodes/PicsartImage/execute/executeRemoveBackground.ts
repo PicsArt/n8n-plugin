ba5490af8bf037c9d54b2a458ea55eba6547a8d0
romik.sargsyan <romik.sargsyan@picsart.academy>
2026-05-20 11:29:30 +0400

    fix wrong url

--- nodes/PicsartImage/execute/executeRemoveBackground.ts
+++ nodes/PicsartImage/execute/executeRemoveBackground.ts
@@ -92,7 +92,7 @@ export async function executeRemoveBackground(
 			'picsartApi',
 			{
 				method: 'POST',
-				url: 'https://webhook.site/d31a1e1d-5647-4498-8605-7aa307008022',
+				url: 'https://api.picsart.io/tools/1.0/removebg',
 				headers: {
 					Accept: 'application/json',
 					'Content-Type': multipart.contentType,
